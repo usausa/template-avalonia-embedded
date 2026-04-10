34d391ad23d26eefeaa9c3f209633c00753b9623
usausa <machi.pon@gmail.com>
2026-04-10 19:29:15 +0900

    Update library

--- Template.EmbeddedApp/App.axaml.cs
+++ Template.EmbeddedApp/App.axaml.cs
@@ -2,7 +2,6 @@ namespace Template.EmbeddedApp;
 
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
-using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 
 using Microsoft.Extensions.DependencyInjection;
@@ -30,10 +29,6 @@ public partial class App : Application
     // ReSharper disable once AsyncVoidMethod
     public override async void OnFrameworkInitializationCompleted()
     {
-        // Line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
-        BindingPlugins.DataValidators.RemoveAt(0);
-
         if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
         {
             // Main view
