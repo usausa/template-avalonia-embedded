31917b311e66f248039ef0b01581936d59a8482b
usausa <machi.pon@gmail.com>
2026-08-31 16:26:28 +0900

    Update

--- Template.EmbeddedApp/Settings/GpioInputSetting.cs
+++ Template.EmbeddedApp/Settings/GpioInputSetting.cs
@@ -1,8 +1,10 @@
 namespace Template.EmbeddedApp.Settings;
 
+#pragma warning disable CA1002
 public sealed class GpioInputSetting
 {
     public List<int> Pins { get; } = [];
 
     public int DebounceMilliseconds { get; set; } = 50;
 }
+#pragma warning restore CA1002
